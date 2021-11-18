cc26aaee9e18d4dd5405c9686e9f90fb1e86a843
Dave <david.mathaga@student.moringaschool.com>
2021-11-18 21:57:17 +0300

    login logout

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,59 +1,60 @@
-# -*- coding: utf-8 -*-
 
-from flask import Blueprint, render_template, redirect, request, current_app, g, flash, url_for
-from flask_login import login_required, logout_user
-from flask_babel import gettext as _
-from .models import User
-from ..extensions import db
-from .forms import SettingsForm
-
-auth = Blueprint('auth', __name__, url_prefix='/auth/', template_folder="templates")
+from flask import render_template, redirect, url_for, flash, request
+from . import auth
+from flask_login import login_user, logout_user, login_required
+from ..models import User
+from .forms import LoginForm, RegistrationForm
+from .. import db
 
 
-@auth.route('login')
-def login():
-    next_url = request.args.get('next') or request.referrer or None
-    return render_template('auth/index.html', next=next_url)
 
-
-@auth.route('loggedin')
-def loggedin():
-    return redirect(request.args.get('next') or url_for('frontend.index'))
-
-
-@auth.route('profile')
-@login_required
-def profile():
-    return render_template('auth/profile.html')
-
-
-@auth.route('set_lang')
-@login_required
-def set_lang():
-    if request.args.get('lang') in current_app.config['LANGUAGES']:
-        user = User.query.get_or_404(g.user.id)
-        user.ui_lang = request.args.get('lang')
-        db.session.add(user)
-        db.session.commit()
-    return redirect('/')
-
-
-@auth.route('settings', methods=['GET', 'POST'])
-@login_required
-def settings():
-    form = SettingsForm(request.form, g.user)
-    form.ui_lang.choices = current_app.config['LANGUAGES'].items()
-
+# register route
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegistrationForm()
     if form.validate_on_submit():
-        form.populate_obj(g.user)
-        db.session.add(g.user)
-        db.session.commit()
-        flash(_("Settings saved"))
+        if User.query.filter_by(email=form.email.data).first():
+            flash('Email already exists', 'danger')
+            return redirect(url_for('auth.signup'))
+        elif User.query.filter_by(username=form.username.data).first():
+            flash('Username already exists', 'danger')
+            return redirect(url_for('auth.signup'))
+        elif len(form.password.data) < 8:
+            flash('Password must be at least 8 characters', 'danger')
+            return redirect(url_for('auth.signup'))
 
-    return render_template('auth/settings.html', languages=current_app.config['LANGUAGES'], form=form)
+        else:
+            user = User(email=form.email.data,
+                        username=form.username.data,
+                        password=form.password.data,name=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            flash('Account created successfully. Please Login', 'success')
+            return redirect(url_for('auth.login'))
+    title = "Create New Account"
+    return render_template('auth/signup.html', form=form, title=title)
 
 
-@auth.route('logout')
+
+
+# login route
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.profile', username=user.username))
+        flash('Invalid username or password', 'danger')
+    title = "Login to your account"
+    return render_template('auth/login.html', form=form, title=title)
+
+
+# logout route
+@auth.route('/logout')
+@login_required
 def logout():
     logout_user()
-    return redirect('/')
+    flash('You have been logged out.', 'success')
+    return redirect(url_for('main.index'))
